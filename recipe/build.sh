9c9c46e82b49f7d7bf19d1ad5dc541409c72f5cd
sophia <scastellarin@quansight.com>
2026-04-22 10:31:20 -0700

    Decrease codegen-units

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,8 @@ set -euxo pipefail
 
 export OPENSSL_DIR=$PREFIX
 
+export RUSTFLAGS="-C codegen-units=8"
+export MATURIN_PEP517_ARGS="-v"
+
 pushd python
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
